35d317bbfa411da042c6df1ee01824f993888144
ian1080 <ijw1080p@gmail.com>
2021-01-31 14:22:09 -0500

    Cleaned up code and added comments

--- titanic.py
+++ titanic.py
@@ -1,24 +1,29 @@
+# Ian Wallace
+# CAP 5610 - Spring 2021
+# Homework 1
+
 import pandas as pd
 import numpy as np
 import random
-
 import matplotlib.pyplot as plt
 
+# Import datasets
 train_df = pd.read_csv('./train.csv')
 test_df = pd.read_csv('./test.csv')
 combine = [train_df, test_df]
 
 
-#define figure and axes
+# Define figure and axes
 fig, ax = plt.subplots()
 
-#hide the axes
+# Hide the axes
 fig.patch.set_visible(False)
 ax.axis('off')
 ax.axis('tight')
 
 # Question 5
 print("Question 5")
+print("==========")
 print("Null values in training data")
 print(train_df.columns[train_df.isnull().any()])
 print("Null values in test data")
@@ -51,6 +56,7 @@ train_df['Pclass'] = pd.Categorical(train_df.Pclass)
 train_df['Sex'] = pd.Categorical(train_df.Sex)
 train_df['Embarked'] = pd.Categorical(train_df.Embarked)
 
+# Get descriptive statistics for each column
 print("Survived")
 print(train_df.Survived.describe())
 
@@ -63,11 +69,32 @@ print(train_df.Sex.describe())
 print("\nEmbarked")
 print(train_df.Embarked.describe())
 
+# Define figure and axes
+fig, ax = plt.subplots()
+
+# Hide the axes
+fig.patch.set_visible(False)
+ax.axis('off')
+ax.axis('tight')
+
+
+# Create table
+des_labels = ['count', 'unique', 'top', 'freq']
+train_df_describe = train_df[['Survived', 'Pclass', 'Sex', 'Embarked']].describe().round(4)
+table = ax.table(cellText=train_df_describe.values, colLabels=train_df_describe.columns, rowLabels=des_labels, loc='center')
+table.set_fontsize(12)
+table.scale(1,2)
+
+plt.savefig('q8.svg')
+plt.savefig('q8.png')
+
+# Question 9
 print("Question 9")
 print("==========")
 
 pclass_survived = list(zip(train_df['Pclass'], train_df['Survived']))
 
+# Caculate the average survival value for each class
 def avg_survived_value_pclass(pclass, data):
     total_p = 0
     num_survived = 0
@@ -86,12 +113,15 @@ print("Class 2:", avg_survived_value_pclass(2, pclass_survived))
 print("Class 3:", avg_survived_value_pclass(3, pclass_survived))
 
 print("Yes we should include this in the model.")
+print()
 
+# Question 10
 print("Question 10")
 print("===========")
 
 sex_survived = list(zip(train_df['Sex'], train_df['Survived']))
 
+# Caculate the average survival value for each sex
 def avg_survived_value_sex(sex, data):
     total_s = 0
     num_survived = 0
@@ -104,15 +134,20 @@ def avg_survived_value_sex(sex, data):
 
     return num_survived/total_s
 
+# Print results
 print("Average Survived Values (by Sex)")
 print("Male:", avg_survived_value_sex('male', sex_survived))
 print("Female:", avg_survived_value_sex('female', sex_survived))
 
 print("Women are more likely to survive.")
+print()
 
+# Question 11
 print("Question 11")
+print("===========")
 
-fig, (ax1, ax2) = plt.subplots(1, 2)
+# Create graph of age vs number survived/died
+fig, (ax1, ax2) = plt.subplots(1, 2, sharex=True, sharey=True)
 
 survived0 = train_df.loc[ (train_df.Survived == 0) ]
 survived1 = train_df.loc[ (train_df.Survived == 1) ]
@@ -122,15 +157,16 @@ survived1['Age'].plot(kind='hist', ax=ax2)
 ax1.set_title('Survived = 0')
 ax2.set_title('Survived = 1')
 
-ax1.set(xlabel='Age', ylabel='Number Survived')
+ax1.set(xlabel='Age', ylabel='Number Died')
 ax2.set(xlabel='Age', ylabel='Number Survived')
 
-ax.legend()
 plt.xlabel('Age')
 plt.savefig('q11.svg')
 #plt.show()
 
+# Qusetion 12
 print("Question 12")
+print("===========")
 
 fig, axs = plt.subplots(3, 2, sharey=True, sharex=True)
 
@@ -152,6 +188,7 @@ p1s1['Age'].plot(ax=axs[0, 1], kind='hist')
 p2s1['Age'].plot(ax=axs[1, 1], kind='hist')
 p3s1['Age'].plot(ax=axs[2, 1], kind='hist')
 
+# Set suplot titles
 axs[0, 0].set_title('Pclass = 1 | Survived = 0')
 axs[1, 0].set_title('Pclass = 2 | Survived = 0')
 axs[2, 0].set_title('Pclass = 3 | Survived = 0')
@@ -163,9 +200,10 @@ plt.savefig('q12.svg')
 #plt.show()
 
 print("Question 13")
+print("===========")
 
+# Set up subplots
 fig, axs = plt.subplots(3, 2)
-
 
 # Filter Data
 g1 = train_df.loc[ (train_df.Survived == 0) & (train_df.Embarked == 'S') ]
@@ -194,7 +232,7 @@ g5_female_avg_fare = g5.loc[ g5.Sex == 'female'].Fare.mean()
 g6_male_avg_fare = g6.loc[ g6.Sex == 'male'].Fare.mean()
 g6_female_avg_fare = g6.loc[ g6.Sex == 'female'].Fare.mean()
 
-
+# Set up data to be plotted
 x = ['male', 'female']
 y1 = [g1_male_avg_fare, g1_female_avg_fare]
 y2 = [g2_male_avg_fare, g2_female_avg_fare]
@@ -203,21 +241,25 @@ y4 = [g4_male_avg_fare, g4_female_avg_fare]
 y5 = [g5_male_avg_fare, g5_female_avg_fare]
 y6 = [g6_male_avg_fare, g6_female_avg_fare]
 
-
+# Generate plots
 plt.subplot(3, 2, 1)
 plt.bar(x, y1)
 plt.yticks([0, 20, 40, 60, 80])
 plt.title('Embarked = S | Survived = 0')
+plt.ylabel('Fare')
 
 plt.subplot(3, 2, 3)
 plt.bar(x, y2)
 plt.yticks([0, 20, 40, 60, 80])
 plt.title('Embarked = C | Survived = 0')
+plt.ylabel('Fare')
 
 plt.subplot(3, 2, 5)
 plt.bar(x, y3)
 plt.yticks([0, 20, 40, 60, 80])
 plt.title('Embarked = Q | Survived = 0')
+plt.xlabel('Sex')
+plt.ylabel('Fare')
 
 plt.subplot(3, 2, 2)
 plt.bar(x, y4)
@@ -233,16 +275,32 @@ plt.subplot(3, 2, 6)
 plt.bar(x, y6)
 plt.yticks([0, 20, 40, 60, 80])
 plt.title('Embarked = Q | Survived = 1')
+plt.xlabel('Sex')
 
+# Save figure and show plot
 plt.savefig('q13.svg')
 plt.tight_layout()
 
 #plt.show()
+print()
 
 print("Question 14")
 print("===========")
 
+# Calculate the number of duplicate values in ticket column
+num_duplicates = train_df.pivot_table(index=['Ticket'], aggfunc='size')
+num_duplicates = len(num_duplicates)
 
+# Calculate total number of values in Ticket
+total_tickets = len(train_df['Ticket'])
+
+# Print Results
+print("Number of duplicate:", num_duplicates)
+print("Number of tickets:", total_tickets)
+print("Duplicate rate:", num_duplicates/total_tickets)
+print()
+
+# Question 15
 print("Question 15")
 print("===========")
 
@@ -250,9 +308,10 @@ print("===========")
 train_cabin_null = train_df['Cabin'].isnull().sum()
 test_cabin_null = test_df['Cabin'].isnull().sum()
 
+# Calculate total number of values in both train and test sets
 total_values = len(train_df['Cabin'].index) + len(test_df['Cabin'].index)
 
-
+# Print results
 print("Null values in training set (Cabin):", train_cabin_null)
 print("Null values in test set (Cabin):", test_cabin_null)
 print("Total null values in Cabin column:", train_cabin_null + test_cabin_null)
@@ -261,16 +320,22 @@ print("Total values in Cabin column:", total_values)
 print( (((train_cabin_null + test_cabin_null)/total_values) * 100).round(2), "percent of the cabin values are null." )
 print()
 
+# Question 16
 print("Question 16")
 print("===========")
 
+# Replace male and female with 0 and 1 respectivly
 train_df['Sex'].replace('male', 0, inplace=True)
 train_df['Sex'].replace('female', 1, inplace=True)
+
+# Rename the Sex column to Gender
 train_df.rename(columns={'Sex':'Gender'}, inplace=True)
 
+# Question 17
 print("Question 17")
 print("===========")
 
+# Calculate the mean and std of Age
 age_mean = train_df.Age.mean()
 age_std = train_df.Age.std()
 
@@ -282,36 +347,54 @@ def get_random_age(mean, std):
 
     return random.randint(lower, upper)
 
+# Show null values
 print("Null values in age (before):", train_df['Age'].isnull().sum())
 
 # Replace all null values in age column with random age values within 1 std of mean
 train_df['Age'] = train_df['Age'].fillna( get_random_age(age_mean, age_std) )
 
+# Show that null values have been filled in
 print("Null values in age (after):", train_df['Age'].isnull().sum())
+print()
 
+# Question 18
 print("Question 18")
 print("===========")
 
+# Show null values
 print("Null values in embarked (before):", train_df['Embarked'].isnull().sum())
+
+# Get the most common Embarked value
+mode = train_df['Embarked'].mode()
+print("The most common embarked value is:")
+print(mode)
 
 # Replace all null values in embarked column with S, it is the most common destination
 train_df['Embarked'] = train_df['Embarked'].fillna('S')
 
+# Show that null values have been filled in
 print("Null values in embarked (after):", train_df['Embarked'].isnull().sum())
+print()
 
+# Question 19
 print("Question 19")
 print("===========")
 
+# Show null values
 print("Null values in fare (before):", test_df['Fare'].isnull().sum())
 
+# Calculate the most common fare value
 test_mode = float(test_df['Fare'].mode())
 print("Mode of fare column in test dataset:", test_mode)
 
 # Replace all null values in fare column with the mode of the column
 test_df['Fare'] = test_df['Fare'].fillna(test_mode)
 
+# Show that null values have been filled in
 print("Null values in fare (after):", test_df['Fare'].isnull().sum())
+print()
 
+# Quesiton 20
 print("Question 20")
 print("===========")
 
@@ -324,7 +407,10 @@ train_df['Fare'].loc[ (train_df['Fare'] > 14.454) & (train_df['Fare'] <= 31.0) ]
 # Ordinal 3
 train_df['Fare'].loc[ (train_df['Fare'] > 31.0)] = 3
 
+# Convert the Fare column to Category instead of float
 train_df['Fare'] = train_df['Fare'].astype('int')
 train_df['Fare'] = train_df['Fare'].astype('category')
 
+# Print descriptive satistics
 print(train_df['Fare'].describe())
+print()
